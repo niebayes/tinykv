8476cb87b52cb76c8803b0fd2208460eb941bc37
lzx <niebayes@gmail.com>
2022-08-26 21:03:29 +0800

    remove local_msg_handlers.go

--- raft/leader_election.go
+++ raft/leader_election.go
@@ -23,6 +23,23 @@ import (
 	"math/rand"
 )
 
+// handle MsgHup message
+func (r *Raft) handleMsgHup(msg pb.Message) {
+	r.becomeCandidate()
+	// if the cluster only contain one node, this node immediately becomes the leader.
+	if len(r.peers) == 1 {
+		r.becomeLeader()
+		return
+	}
+	// otherwise, there must be a majority of votes from the cluster to become the leader.
+	r.bcastRequestVote()
+}
+
+// handle MsgBeat message.
+func (r *Raft) handleBeat(msg pb.Message) {
+	r.bcastHeartbeat()
+}
+
 // becomeFollower transform this peer's state to Follower
 // @param term
 // @param lead the node with id lead is believed to be the current leader.
